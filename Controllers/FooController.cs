08124c1dafce3c499ac0f69958d3507a754ca414
Justin Kaldenbach <justin@gooddonegreat.com>
2016-02-15 15:12:11 -0600

    inital commit

---
+++ Controllers/FooController.cs
@@ -0,0 +1,32 @@
+using dotnet_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace dotnet_api.Controllers
+{
+  [Route("api/[controller]")]
+  public class FooController : Controller
+  {
+    static List<Foo> foos = new List<Foo>();
+    static int nextId = 1;
+
+    //Get: api/foo
+    [HttpGet]
+    public IEnumerable<Foo> Get()
+    {
+      return foos;
+    }
+
+    //Get: api/foo/name
+    [HttpGet("{name}")]
+    public IEnumerable<Foo> Insert(string name)
+    {
+      foos.Add(new Foo { id = nextId, name = name });
+      nextId++;
+      return foos;
+    }
+  }
+}
---
+++ Models/FooModel.cs
@@ -0,0 +1,8 @@
+namespace dotnet_api.Models
+{
+    public class Foo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
